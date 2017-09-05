051a26875be3f76ca5e9e845978caa3827ada2cf
shuaiZend <shuai.zend@qq.com>
2017-09-05 10:45:30 +0800

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,9 +2,8 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Homepage</title>
-
-  <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+  <title>System Index</title>
+  <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 </head>
 <body>
   <div id="app"></div>
