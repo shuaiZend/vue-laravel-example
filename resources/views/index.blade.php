46f8f436a6afa376e657512a5408656e6ac3945e
shuaiZend <shuai.zend@qq.com>
2017-09-05 15:07:40 +0800

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>System Index</title>
+  <title>Browser System Index</title>
   <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 </head>
 <body>
