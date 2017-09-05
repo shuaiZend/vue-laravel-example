a6a024ea57ed9646ca2177f8f681eae5ea63053a
shuaiZend <shuai.zend@qq.com>
2017-09-05 17:36:10 +0800

    Update data.js

--- public/js/data.js
+++ public/js/data.js
@@ -1,13 +1,15 @@
 {  
 	"List": {  
-		"康程管理系统":{
+		"System":{
+			"Name":"康程管理系统",
 			"Url": "http://202.201.201.251/Login",
 			"Introduce":"医院等级评审管理,院内文件管理",
 			"Image":"https://b-ssl.duitang.com/uploads/item/201212/02/20121202153422_Uancu.jpeg",
 			"Contact":"User",
 			"Password":"admin@1234"
 		},
-		"三基考试系统":{
+		"System":{
+			"Name":"三基考试系统",
 			"Url": "http://202.201.201.20:8001/",
 			"Introduce":"三基考试系统",
 			"Image":"https://b-ssl.duitang.com/uploads/item/201212/02/20121202153335_BXNRM.thumb.700_0.jpeg",
