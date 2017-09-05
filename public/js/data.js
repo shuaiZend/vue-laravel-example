7297e2900e67bf1a89d37c1114d761c68208a340
shuaiZend <shuai.zend@qq.com>
2017-09-05 16:41:40 +0800

    Update data.js

--- public/js/data.js
+++ public/js/data.js
@@ -3,14 +3,14 @@
 		"康程管理系统":{
 			"Url": "http://202.201.201.251/Login",
 			"Introduce":"医院等级评审管理,院内文件管理",
-			"Image":"",
+			"Image":"https://b-ssl.duitang.com/uploads/item/201212/02/20121202153422_Uancu.jpeg",
 			"Contact":"User",
 			"Password":"admin@1234"
 		},
 		"三基考试系统":{
 			"Url": "http://202.201.201.20:8001/",
 			"Introduce":"三基考试系统",
-			"Image":"",
+			"Image":"https://b-ssl.duitang.com/uploads/item/201212/02/20121202153335_BXNRM.thumb.700_0.jpeg",
 			"Contact":"小赵@17783695801",
 			"Password":"admin@cdhtyy"
 		}
